d8f93d27ccf75a3cd0b69a27ae91216d5ddbd561
Alexander Mandrov <45426001+Unsleeping@users.noreply.github.com>
2020-04-05 09:37:16 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,18 @@
-from django.urls import path  # из библиотеки django.urls импортируем функцию path
-from . import views  # импортируем файл views.py из корня проекта
+from django.urls import path 
+# переназовём файлы views.py, чтобы избежать конфликта имён.
+# импортируем views.py из папки приложения Articles App:
+from articles import views as art_views # ...и будем звать его art_views
 
-urlpatterns = [
-    path('', views.index)
+# импортируем views.py из папки приложения Accounts App:
+from accounts import views as acc_views # ...и будем звать его acc_views 
+
+# импортируем views.py из директории по умолчанию:
+from . import views # ...а давать ей специальное имя не будем, оставим views
+
+urlpatterns = [ 
+    path('', views.index), 
+    path('dashboard', art_views.dashboard), 
+    path('sign_up', acc_views.sign_up), 
+    path('sign_in', acc_views.sign_in), 
+    path('my_account', acc_views.my_account), 
 ]
