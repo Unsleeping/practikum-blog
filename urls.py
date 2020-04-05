6cfbab51c1c907e95654052edc02b928e591912b
Alexander Mandrov <45426001+Unsleeping@users.noreply.github.com>
2020-04-05 09:42:09 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,18 +1,15 @@
-from django.urls import path 
-# переназовём файлы views.py, чтобы избежать конфликта имён.
-# импортируем views.py из папки приложения Articles App:
-from articles import views as art_views # ...и будем звать его art_views
+# импортируем функцию для проверки запрошенных URL
+from django.urls import path  
+# импортируем все три файла с view-функциями из разных директорий
+from accounts import views as acc_views
+from articles import views as art_views 
+from . import views
 
-# импортируем views.py из папки приложения Accounts App:
-from accounts import views as acc_views # ...и будем звать его acc_views 
-
-# импортируем views.py из директории по умолчанию:
-from . import views # ...а давать ей специальное имя не будем, оставим views
-
-urlpatterns = [ 
-    path('', views.index), 
-    path('dashboard', art_views.dashboard), 
-    path('sign_up', acc_views.sign_up), 
-    path('sign_in', acc_views.sign_in), 
-    path('my_account', acc_views.my_account), 
+urlpatterns = [
+     # правила для сопоставления шаблонов URL и функций
+     path('', views.index),
+     path('articles/dashboard', art_views.dashboard),
+     path('accounts/sign-up', acc_views.sign_up),
+     path('accounts/sign-in', acc_views.sign_in),
+     path('accounts/my-account', acc_views.my_account)
 ]
